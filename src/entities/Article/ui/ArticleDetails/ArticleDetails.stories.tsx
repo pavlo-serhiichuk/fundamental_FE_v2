b00da2962d43a05c0413d40b22ed4ca67d545c06
Pavlo Serhiichuk <pavel.serhiichuk@gmail.com>
2024-09-10 16:24:18 +0300

    fixed stories displaying

--- src/entities/Article/ui/ArticleDetails/ArticleDetails.stories.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.stories.tsx
@@ -1,12 +1,15 @@
 import { type Meta, type StoryObj } from '@storybook/react'
 import { ArticleDetails } from './ArticleDetails'
 import { StoreDecorator } from 'shared/lib/storybook/decorators'
+import { getArticleMockState } from 'entities/Article/model/slice/articleState'
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const mockData: any = getArticleMockState()
+
 const meta = {
   title: 'entities/ArticleDetails',
   component: ArticleDetails,
-  decorators: [StoreDecorator({ articleDetails: {} })]
+  decorators: [StoreDecorator({ articleDetails: mockData })]
 } as Meta<typeof ArticleDetails>
 
 export default meta
